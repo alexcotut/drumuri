ea9525173712b92c55eec9f5f8b5886f4b169d4b
Daruk <metalxster@gmail.com>
2025-11-25 22:02:48 +0200

    Added way info box

--- webserver/src/js/index2.js
+++ webserver/src/js/index2.js
@@ -1,10 +1,11 @@
 import "./maplibre-gl.js";
 
 let map;
+let infoBox;
 
 window.addEventListener('load', () => {
 
-    keepAwake();
+    // keepAwake();
 
     map = new maplibregl.Map({
         container: 'map', // container id
@@ -22,19 +23,43 @@ window.addEventListener('load', () => {
         ]
     }));
 
+    map.addControl(new maplibregl.NavigationControl(), 'top-right')
+
+    map.on('click', mapClick);
+
+    infoBox = document.querySelector('#info-box');
 });
 
 
+function mapClick(e) {
+    const features = map.queryRenderedFeatures([
+        [e.point.x - 3, e.point.y - 3],
+        [e.point.x + 3, e.point.y + 3]
+    ], 
+    { 
+        layers: [
+            'tunnel_motorway_inner',
+            'highway_minor',
+            'highway_major_inner',
+            "highway_motorway_inner"
+        ] 
+    });
 
-async function keepAwake() {
-    let wakeLock = null;
 
-    try {
-        wakeLock = await navigator.wakeLock.request("screen");
-        console.log("Wake Lock is active!");
-    } catch (err) {
-        // The Wake Lock request has failed - usually system related, such as battery.
-        console.log(`${err.name}, ${err.message}`);
-    }
+    infoBox.innerHTML = '';
+    features.forEach(f => {
+        const item = createListItem(f);
+        if (item !== null) infoBox.appendChild(item);
+    });
+    
+}
 
+function createListItem(feature) {
+    if (feature.properties.ref === undefined) return null;
+    const e = document.createElement('div');
+    e.className = 'list-item';
+    e.innerHTML = `<span>${feature.properties.ref}</span>
+        <span>${feature.properties.smoothness === undefined ? '--' : feature.properties.smoothness}</span>`;
+    console.log(feature.properties.way);
+    return e;
 }
